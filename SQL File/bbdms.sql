286ac46d7a159350d85b561b5ff49052c7310f7f
Sklita <ishalsklita@gmail.com>
2022-12-14 14:45:35 +0530

    commit 6

--- SQL File/bbdms.sql
+++ SQL File/bbdms.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3399:3399
--- Generation Time: Nov 30, 2022 at 11:32 AM
+-- Generation Time: Dec 14, 2022 at 10:12 AM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 7.4.30
 
@@ -95,7 +95,7 @@ INSERT INTO `tblcontactusquery` (`id`, `name`, `EmailId`, `ContactNumber`, `Mess
 
 CREATE TABLE `tbldeletion` (
   `ID` int(10) NOT NULL,
-  `name` varchar(250) DEFAULT NULL,
+  `name` varchar(100) DEFAULT NULL,
   `EmailId` varchar(250) DEFAULT NULL,
   `ContactNumber` bigint(10) DEFAULT NULL,
   `deletefor` varchar(250) DEFAULT NULL,
@@ -109,7 +109,16 @@ CREATE TABLE `tbldeletion` (
 --
 
 INSERT INTO `tbldeletion` (`ID`, `name`, `EmailId`, `ContactNumber`, `deletefor`, `hideordelete`, `Message`, `ApplyDate`) VALUES
-(8, 'priya', 'priya@gmail.com', 9312524698, 'Others', 'Hide', 'I BBB', '2022-11-30 10:24:27');
+(12, 'priya', 'priya@gmail.com', 7896541233, 'No more Donation Available', 'DeleteMyAccount', 'delete my account as i have donated', '2022-12-07 15:44:54'),
+(14, 'priya', 'priya@gmail.com', 7896541233, 'Others', 'DeleteMyAccount', ',mmmmmmmmmmmmmmm', '2022-12-09 16:35:03');
+
+--
+-- Triggers `tbldeletion`
+--
+DELIMITER $$
+CREATE TRIGGER `deletion` AFTER DELETE ON `tbldeletion` FOR EACH ROW DELETE FROM tbldonars WHERE FullName=OLD.name
+$$
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -122,7 +131,7 @@ CREATE TABLE `tbldonars` (
   `FullName` varchar(100) DEFAULT NULL,
   `MobileNumber` char(11) DEFAULT NULL,
   `EmailId` varchar(100) DEFAULT NULL,
-  `Gender` varchar(20) DEFAULT NULL,
+  `Donation` varchar(20) DEFAULT NULL,
   `Age` int(11) DEFAULT NULL,
   `DonationGroup` varchar(20) DEFAULT NULL,
   `Address` varchar(255) DEFAULT NULL,
@@ -136,7 +145,7 @@ CREATE TABLE `tbldonars` (
 -- Dumping data for table `tbldonars`
 --
 
-INSERT INTO `tbldonars` (`id`, `FullName`, `MobileNumber`, `EmailId`, `Gender`, `Age`, `DonationGroup`, `Address`, `Message`, `PostingDate`, `status`, `Password`) VALUES
+INSERT INTO `tbldonars` (`id`, `FullName`, `MobileNumber`, `EmailId`, `Donation`, `Age`, `DonationGroup`, `Address`, `Message`, `PostingDate`, `status`, `Password`) VALUES
 (6, 'Ashu Misra', '7797987981', 'ashu@gmail.com', 'Student Calculator ', 35, 'Gadgets', 'H-900, Ravinderpuri colony, Varanasi', ' Call me if Calculators are required I have 2 extra Calculators', '2022-05-17 12:43:41', 1, '202cb962ac59075b964b07152d234b70'),
 (9, 'Test Demo', '9789797979', 'test@gmail.com', 'Mugs Vessels', 56, 'Vessels', 'Allahabad', ' gjhgjhgjhghjghj', '2022-05-21 06:09:08', 1, '202cb962ac59075b964b07152d234b70'),
 (10, 'John Doe', '1236547890', 'johnd@gmail.com', 'Sports Shoes', 25, 'Footwear', 'A 5623 XYZ Street New Delhi', 'Footwear for needy sports students , new one of size 7', '2022-07-29 01:50:58', 1, 'f925916e2754e5e03f75dd58a5733251'),
@@ -145,10 +154,10 @@ INSERT INTO `tbldonars` (`id`, `FullName`, `MobileNumber`, `EmailId`, `Gender`, 
 (15, 'Aishu', '9988776655', 'aishu@gmail.com', 'Engineering Mathemat', 26, 'Stationary', 'Mysore', ' I would like to donate books for needy ', '2022-11-21 16:01:44', 1, 'f598f436f5a2bb404404acd8568526fb'),
 (16, 'ambani', '7856941239', 'ambani@gmail.com', 'Computers', 59, 'Gadgets', 'Mumbai', ' I want to help needy engineering students', '2022-11-23 11:15:02', 1, 'f454c7b96c33c5946d120e2e88330d52'),
 (17, 'Alex', '9632145879', 'alex@gmail.com', 'Cooker and Grinder', 30, 'Vessels', 'Bengaluru', ' I want to donate the vessels for ashramas ', '2022-11-24 11:49:44', 1, 'b7e6124aeffd2498bdb4fb8c525c6ea3'),
-(18, 'priya', '9856321479', 'priya@gmail.com', 'Notebook Navaneet 20', 26, 'Stationary', ' Srirampur Mysuru', ' I ant to Notebook Navaneet 200 pages 2 dozen books for needy students They are new books ', '2022-11-26 07:13:38', 0, 'cfb077657c9b746245f0e9106c0f7660'),
 (19, 'Saniya', '7856491239', 'saniya@gmail.com', 'Sweaters for needy ', 28, 'Clothes', 'Mangalore', ' I want to donate clothes like sweaters and jackets for needy ', '2022-11-29 06:21:27', 1, 'a0260154fe07352e36d3a432df569ad9'),
 (20, 'Bob', '1236547899', 'bob@gmail.com', 'Packed food ', 30, 'Food', 'Hubli', ' I want to donate packed food for needy ', '2022-11-29 06:25:24', 1, '5dda79243ec39dbdf6dd58b136149a08'),
-(21, 'Mohan ', '7896541233', 'mohan@gmail.com', 'Clothes Like Pants a', 52, 'Clothes', 'Mangalore', ' ', '2022-11-30 07:08:37', 1, '1aa79deaf05f0fe00a5d46b88145db92');
+(21, 'Mohan ', '7896541233', 'mohan@gmail.com', 'Clothes Like Pants a', 52, 'Clothes', 'Mangalore', ' ', '2022-11-30 07:08:37', 1, '1aa79deaf05f0fe00a5d46b88145db92'),
+(26, 'priya', '7896541233', 'priya@gmail.com', 'Food Groceries ', 30, 'Food', 'Kodagu', 'Ready to donate groceries for Needy people ', '2022-12-07 15:43:10', 1, 'cfb077657c9b746245f0e9106c0f7660');
 
 -- --------------------------------------------------------
 
@@ -203,7 +212,7 @@ INSERT INTO `tblpages` (`id`, `PageName`, `type`, `detail`) VALUES
 
 CREATE TABLE `tblrequirer` (
   `ID` int(10) NOT NULL,
-  `BloodDonarID` int(10) DEFAULT NULL,
+  `DonarID` int(10) DEFAULT NULL,
   `name` varchar(250) DEFAULT NULL,
   `EmailId` varchar(250) DEFAULT NULL,
   `ContactNumber` bigint(10) DEFAULT NULL,
@@ -216,18 +225,20 @@ CREATE TABLE `tblrequirer` (
 -- Dumping data for table `tblrequirer`
 --
 
-INSERT INTO `tblrequirer` (`ID`, `BloodDonarID`, `name`, `EmailId`, `ContactNumber`, `BloodRequirefor`, `Message`, `ApplyDate`) VALUES
+INSERT INTO `tblrequirer` (`ID`, `DonarID`, `name`, `EmailId`, `ContactNumber`, `BloodRequirefor`, `Message`, `ApplyDate`) VALUES
 (1, 6, 'Rakesh', 'rak@gmail.com', 7894561236, 'Father', 'Please help', '2022-05-17 11:57:24'),
 (2, 3, 'Mukesh', 'muk@gmail.com', 5896231478, 'Others', 'Please help', '2022-05-17 11:58:48'),
 (3, 6, 'Hitesh', 'hit@gmail.com', 1236547896, 'Brother', 'do the needful', '2022-05-17 12:02:12'),
 (4, 10, 'Rahul Singh', 'rahk@gmail.com', 2536251425, 'Mother', 'Please help me', '2022-07-29 01:51:52'),
-(5, 11, 'Anuj Kumar', 'ak@gmail.com', 8525232102, 'Others', 'Need blood on urgent basis', '2022-08-02 01:24:18'),
 (6, 12, 'priya', 'priya@gmail.com', 5469871236, 'Mother', 'i REQUIRE THIS ', '2022-11-17 04:01:06'),
 (7, 14, 'xyz', 'xyz@gmail.com', 9856321145, 'Others', 'I want for orphange', '2022-11-17 09:53:44'),
 (8, 13, 'preethi', 'preethi@gamil.com', 3214569877, 'Mother', 'xyz', '2022-11-23 10:58:36'),
 (9, 13, 'sahana', 'sahana@gmail.com', 2365987412, 'Orphanage', 'for orphans', '2022-11-23 11:11:02'),
 (10, 17, 'sadana', 'sadana@gmail.com', 7896541239, 'Others', 'I want vessels to donate for neighbor\'s who require this ', '2022-11-24 12:05:58'),
-(11, 18, 'sahana', 'sahana@gmail.com', 1236547891, 'Personal Usage', 'I want these books for my studies I\'m a student it would be helpful if you can give me this ', '2022-11-26 07:16:23');
+(11, 18, 'sahana', 'sahana@gmail.com', 1236547891, 'Personal Usage', 'I want these books for my studies I\'m a student it would be helpful if you can give me this ', '2022-11-26 07:16:23'),
+(12, 26, 'susheela', 'susheela@gmail.com', 1236547888, 'Personal Usage', 'I need this please ', '2022-12-10 05:47:49'),
+(13, 20, 'preksha', 'preksha@gmail.com', 1236547899, 'Needy Ones', 'i need this ', '2022-12-10 06:08:06'),
+(14, 12, 'preethi', 'preethi@gamil.com', 78965412336, 'Others', 'I want This ', '2022-12-11 04:39:33');
 
 --
 -- Indexes for dumped tables
@@ -301,19 +312,19 @@ ALTER TABLE `tblcontactusinfo`
 -- AUTO_INCREMENT for table `tblcontactusquery`
 --
 ALTER TABLE `tblcontactusquery`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `tbldeletion`
 --
 ALTER TABLE `tbldeletion`
-  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `tbldonars`
 --
 ALTER TABLE `tbldonars`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
 -- AUTO_INCREMENT for table `tblgroup`
@@ -331,7 +342,7 @@ ALTER TABLE `tblpages`
 -- AUTO_INCREMENT for table `tblrequirer`
 --
 ALTER TABLE `tblrequirer`
-  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
